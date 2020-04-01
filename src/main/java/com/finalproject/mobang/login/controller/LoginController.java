32f6a770fa7104b613de299a7e76ae79cc9de277
djkim1216 <primarydj@daum.net>
2020-04-01 09:58:55 +0900

    insert login, roomsearch update

--- src/main/java/com/finalproject/mobang/login/controller/LoginController.java
+++ src/main/java/com/finalproject/mobang/login/controller/LoginController.java
@@ -1,6 +1,33 @@
 package com.finalproject.mobang.login.controller;
 
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.finalproject.mobang.user.biz.roomsearchBiz;
+import com.finalproject.mobang.user.controller.HomeController;
+
+@Controller
 public class LoginController {
 
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@RequestMapping(value = "/login.all")
+	public String home(Locale locale, Model model) {
+		logger.info("login");
+
+		return "login/login";
+	}
+	
+	@RequestMapping(value = "/access_denied_page.all")
+	public String access_denied_page(Locale locale, Model model) {
+		logger.info("login");
+
+		return "login/access_denied_page";
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/login/access_denied_page.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
+</head>
+<body>
+	<h1><i class="fas fa-user-slash">권한이 없습니다 <br/><br/> 관리자에게 문의해주세요</i></h1>
+</body>
+</html>
